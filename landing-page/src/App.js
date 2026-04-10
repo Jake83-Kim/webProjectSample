a7a1d0033023a6148cd4f433adda1c9e6734574b
Jake83-Kim <linkarl1002@gmail.com>
2026-04-10 14:46:55 +0900

    Auto commit: Update App.js and add about.html

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
 function App() {
+  const handleLearnMore = () => {
+    window.open('/about.html', '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>안녕하세요, [이름]입니다.</h1>
-        <p>저는 [직업/관심사]입니다. 이곳은 제 소개 페이지입니다.</p>
-        <button>더 알아보기</button>
+        <h1>안녕하세요, [JAKE]입니다.</h1>
+        <button type="button" onClick={handleLearnMore}>
+          더 알아보기
+        </button>
       </header>
     </div>
   );
