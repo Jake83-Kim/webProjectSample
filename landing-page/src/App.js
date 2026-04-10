e19af4379fbf3db69442728c86f5834d44b0e4d1
Jake83-Kim <linkarl1002@gmail.com>
2026-04-10 15:02:37 +0900

    Update button to '사장님 페이지' and create owner.html dashboard

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 
 function App() {
-  const handleLearnMore = () => {
-    window.open(`${process.env.PUBLIC_URL}/about.html`, '_blank', 'noopener,noreferrer');
+  const handleOwnerPage = () => {
+    window.open(`${process.env.PUBLIC_URL}/owner.html`, '_blank', 'noopener,noreferrer');
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>안녕하세요, [JAKE]입니다.</h1>
-        <button type="button" onClick={handleLearnMore}>
-          더 알아보기
+        <button type="button" onClick={handleOwnerPage}>
+          사장님 페이지
         </button>
       </header>
     </div>
