1d359c3e523c0eb8af7cddbd535b35bb4cd3bb22
PLANNING_JAKE\kjhph <snail@rocateer.com>
2026-04-10 13:57:01 +0900

    V0.1

---
+++ landing-page/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>안녕하세요, [이름]입니다.</h1>
+        <p>저는 [직업/관심사]입니다. 이곳은 제 소개 페이지입니다.</p>
+        <button>더 알아보기</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
